d5866fb10ec8afb5314870bae2aa30e48dafd522
aires <jpaulo.aires@gmail.com>
2017-06-22 14:19:12 -0300

    Changing constants
    Changed the constants so they can be used anywhere.

--- conconexp/home/constants.py
+++ conconexp/home/constants.py
@@ -1,16 +1,19 @@
+import os
 from sklearn.feature_extraction.text import CountVectorizer
 from norm_identifier.sentence_classifier import SentenceClassifier
+from django.conf import settings
 
+base_dir = settings.BASE_DIR
 BASE_FOLDER_PATH = 'dataset/'
-CLASSIFIER_PATH = '/home/lsa/conconexp/home/norm_identifier/classifiers/' \
+CLASSIFIER_PATH = os.path.join(base_dir, 'home/norm_identifier/classifiers/' \
                   '16-10-25_12:18:39/' \
-                  'sentence_classifier_16-10-25_12:18:39.pkl'
-PATH_TO_WEIGHTS = '/home/lsa/conconexp/home/model/model.h5'
-PATH_TO_MODEL = '/home/lsa/conconexp/home/model/model.json'
-NAMES_PATH = '/home/lsa/conconexp/home/norm_identifier/classifiers/' \
-             '16-10-25_12:18:39/sentence_classifier_16-10-25_12:18:39_names.txt'
+                  'sentence_classifier_16-10-25_12:18:39.pkl')
+PATH_TO_WEIGHTS = os.path.join(base_dir, 'home/model/model.h5')
+PATH_TO_MODEL = os.path.join(base_dir, 'home/model/model.json')
+NAMES_PATH = os.path.join(base_dir, 'home/norm_identifier/classifiers/' \
+             '16-10-25_12:18:39/sentence_classifier_16-10-25_12:18:39_names.txt')
 MAX_LENGTH = 200
-WEIGHTS = '/home/lsa/conconexp/home/model/weights.hdf5'
+WEIGHTS = os.path.join(base_dir, 'home/model/weights.hdf5')
 
 
 def get_classifier():
